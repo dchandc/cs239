d8f523501940eabaffcb4e3b4af6b5e61fe2ab20
Dennis Chan <dchan@gohigherup.com>
2016-02-28 22:37:19 -0800

    Add a bit more parsing

--- create_csv.py
+++ create_csv.py
@@ -2,9 +2,26 @@
 
 import argparse, re, sys
 
+class MethodStats:
+	def __init__(self, name):
+		self.name = name
+		self.numPassed = 0
+		self.numFailed = 0
+
 parser = argparse.ArgumentParser()
 parser.add_argument('output_file', type=argparse.FileType('r'), help='file containing method calls and test results')
 args = parser.parse_args()
 
+line_index = 0
 for line in args.output_file:
-	print line
+	line_index += 1
+	fields = line.strip().split()
+	if len(fields) != 2:
+		if len(fields) > 0:
+			print 'malformed line[{0}]: {1}'.format(line_index, line)
+		continue
+
+	if fields[0] == '[Call]':
+		print 'call'
+	elif fields[0] == '**TestResult:':
+		print 'result'
