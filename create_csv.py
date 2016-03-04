aa656fcca9705e7950808c61ba1b3ef7ff65d2b3
Dennis Chan <dchan@gohigherup.com>
2016-03-03 21:58:47 -0800

    Sort methods by class

--- create_csv.py
+++ create_csv.py
@@ -34,11 +34,12 @@ class MethodStats:
 	num_passed = 0
 	num_failed = 0
 
-	def __init__(self, name):
-		self.name = name
+	def __init__(self, class_name, method_name):
+		self.class_name = class_name
+		self.method_name = method_name
 
 	def __repr__(self):
-		return '<' + self.name + ', passed: ' + str(self.num_passed) + ', failed: ' + str(self.num_failed) + '>'
+		return '<' + self.class_name + '::' + self.method_name + ', passed: ' + str(self.num_passed) + ', failed: ' + str(self.num_failed) + '>'
 
 parser = argparse.ArgumentParser()
 parser.add_argument('output_file', type=argparse.FileType('r'), help='file containing method calls and test results')
@@ -58,29 +59,39 @@ for line in args.output_file:
 		continue
 
 	if fields[0] == '[Call]':
-		if fields[1] not in current_method_stats:
-			current_method_stats[fields[1]] = 1
+		lst = fields[1].split('::')
+		if lst[1] == '<init>':
+			continue
+		if lst[0] not in current_method_stats:
+			current_method_stats[lst[0]] = {lst[1]: 1}
+		else:
+			current_method_stats[lst[0]][lst[1]] = 1
 	elif fields[0] == '**TestResult:':
 		if fields[1] == 'Pass':
 			total_passed += 1
 		elif fields[1] == 'Fail':
 			total_failed += 1
 
-		for key in current_method_stats.keys():
-			if key not in all_method_stats:
-				all_method_stats[key] = MethodStats(key)
+		for class_name in current_method_stats.keys():
+			if class_name not in all_method_stats:
+				all_method_stats[class_name] = {}
 
-			if fields[1] == 'Pass':
-				all_method_stats[key].num_passed += 1 
-			elif fields[1] == 'Fail':
-				all_method_stats[key].num_failed += 1 
+			for method_name in current_method_stats[class_name].keys():
+				if method_name not in all_method_stats[class_name]:
+					all_method_stats[class_name][method_name] = MethodStats(class_name, method_name)
+
+				if fields[1] == 'Pass':
+					all_method_stats[class_name][method_name].num_passed += 1
+				elif fields[1] == 'Fail':
+					all_method_stats[class_name][method_name].num_failed += 1
 		current_method_stats = {}
 
 with open('scores.csv', 'w+') as f:
-	for key in all_method_stats:
-		print all_method_stats[key]
-		stats = all_method_stats[key]
-		scores = calculateSS(stats.num_passed, total_passed, stats.num_failed, total_failed)
-		f.write('{0},{1},{2},{3},{4},{6:.2f},{6:.2f},{7:.2f},{8:.2f}\n'.format(key, stats.num_passed, total_passed, stats.num_failed, total_failed, scores['Tarantula'], scores['SBI'], scores['Jaccard'], scores['Ochiai']))
+	for class_name in all_method_stats:
+		for method_name in all_method_stats[class_name]:
+			print all_method_stats[class_name][method_name]
+			method_stats = all_method_stats[class_name][method_name]
+			scores = calculateSS(method_stats.num_passed, total_passed, method_stats.num_failed, total_failed)
+			f.write('{0},{1},{2},{3},{4},{5},{6:.2f},{7:.2f},{8:.2f},{9:.2f}\n'.format(class_name, method_name, method_stats.num_passed, total_passed, method_stats.num_failed, total_failed, scores['Tarantula'], scores['SBI'], scores['Jaccard'], scores['Ochiai']))
 
 print 'Totals - passed: ' + str(total_passed) + ', failed: ' + str(total_failed)
